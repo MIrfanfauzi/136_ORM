d95e65283db73f1a2bae01396987beaeb0d82fee
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-10-29 11:39:28 +0700

    menambahkan endpoint post komik

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ db.sequelize.sync()
         console.log(err);
     })
 
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (error) {}
+    res.send({ message: error.message });
+});
